4e0f69ea5c18039f451aaa55059baced358f7d6e
bekogeko <gulestanbekir@gmail.com>
2022-07-22 01:48:08 +0300

    Remove Unused component from index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,6 @@ import Head from "next/head";
 import Link from "next/link";
 import { trpc } from "../utils/trpc";
 
-type TechnologyCardProps = {
-  name: string;
-  description: string;
-  documentation: string;
-};
-
 const Home: NextPage = () => {
   const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
 
@@ -27,34 +21,13 @@ const Home: NextPage = () => {
         <p className="text-lg text-gray-700">
           You can still go to{" "}
           <Link href="/dashboard">
-            <p className="text-blue-600 inline-block cursor-pointer">
+            <a className="text-blue-600 inline-block cursor-pointer">
               Dashboard
-            </p>
+            </a>
           </Link>
         </p>
       </main>
     </>
-  );
-};
-
-const TechnologyCard = ({
-  name,
-  description,
-  documentation,
-}: TechnologyCardProps) => {
-  return (
-    <section className="flex flex-col justify-center p-6 duration-500 border-2 border-gray-500 rounded shadow-xl motion-safe:hover:scale-105">
-      <h2 className="text-lg text-gray-700">{name}</h2>
-      <p className="text-sm text-gray-600">{description}</p>
-      <a
-        className="mt-3 text-sm underline text-violet-500 decoration-dotted underline-offset-2"
-        href={documentation}
-        target="_blank"
-        rel="noreferrer"
-      >
-        Documentation
-      </a>
-    </section>
   );
 };
 
