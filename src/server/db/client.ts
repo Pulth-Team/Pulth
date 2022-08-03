7b590499becffe8968d36cf556f3a46d5b21499f
bekogeko <gulestanbekir@gmail.com>
2022-08-03 22:38:53 +0300

    Change BasicLoggin from verbose to only reqired

--- src/server/db/client.ts
+++ src/server/db/client.ts
@@ -9,7 +9,8 @@ declare global {
 export const prisma =
   global.prisma ||
   new PrismaClient({
-    log: ["query"],
+    // log: ["query"],
+    log: ["info", "warn", "error"],
   });
 
 if (env.NODE_ENV !== "production") {
