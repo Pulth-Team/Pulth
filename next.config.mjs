6ac8663d28e63674fb0ec95c5bcf2a21c64e87b5
Bekogeko <gulestanbekir@gmail.com>
2022-12-13 03:57:36 +0300

    Init Order

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,17 @@ import { withSentryConfig } from "@sentry/nextjs";
 function defineNextConfig(config) {
   return config;
 }
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
 
 export default withSentryConfig(
   defineNextConfig({
@@ -32,15 +43,3 @@ export default withSentryConfig(
   }),
   sentryWebpackPluginOptions
 );
-
-const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
-
-  silent: true, // Suppresses all logs
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-};
