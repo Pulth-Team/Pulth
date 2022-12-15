08e73981ef6c63562018be84a8197c09c0d00190
Bekogeko <gulestanbekir@gmail.com>
2022-12-15 05:33:59 +0300

    Comment and Modulize EditorComponent

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,28 +1,36 @@
 import EditorJS from "@editorjs/editorjs";
+import { NextPage } from "next";
 import React, { useState, useEffect, useRef, useId } from "react";
 
-const Editor = () => {
+const Editor: NextPage<{ className?: string }> = ({ className }) => {
   const id = useId();
   const [editor, setEditor] = useState<EditorJS | null>(null);
 
   useEffect(() => {
-    console.log("hii");
+    // set editor only if it's not set
     setEditor((prevEditor) => {
+      // if editor is not set, create new editor
       if (!prevEditor) {
         return new EditorJS({
           holder: id,
         });
       }
+      // if editor is set, return null
       return null;
     });
+
+    // destroy editor on unmount
     return () => {
+      // destroy editor if it's set
       if (editor) {
         editor.destroy();
       }
     };
+
+    // run effect only if id changes
   }, [editor, id]);
 
-  return <div id={id} className="border border-gray-400"></div>;
+  return <div id={id} className={className}></div>;
 };
 
 export default Editor;
