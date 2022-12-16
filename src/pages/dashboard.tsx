c7e085cce377f6e3f7735cb98ce45fcf747c7ad3
Bekogeko <gulestanbekir@gmail.com>
2022-12-16 06:41:36 +0300

    Add AST Abstraction And Validity Utils
    Added AST (Abstract Syntax Tree) to check and validate Inline Tool Output for security adn later renderer feature.
    
    Models Changed for API next rework on editor

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,18 +1,44 @@
 import type { NextPage } from "next";
 
 import { trpc } from "../utils/trpc";
+import {
+  getASTfromHTML,
+  PureElementAST,
+  ValidateTree,
+} from "../utils/editorHelpers";
 import Head from "next/head";
 import Link from "next/link";
 
 import { useSession } from "next-auth/react";
 
 import DashboardLayout from "../components/layouts/dashboard";
+import { useState } from "react";
 
 const Dashboard: NextPage = () => {
   // const batchFetch = trpc.useQuery(["article.batch-data"]);
 
   const { data } = useSession();
   const user = data?.user;
+
+  const [parsed, setParsed] = useState<any>("");
+  const [validity, setValidity] = useState<any>("");
+  const InlineOutput =
+    // 'generate an <code class="inline-ode">audio version</code> and so on.';
+    'workspace consists of separate<mark class="cdx-marker"> Blocks: paragraphs,</mark> headings<mark class="cdx-marker">, images, <i>lists</i>, quotes,</mark> etc.&nbsp;';
+
+  const Parse = () => {
+    setValidity("");
+    const AST = getASTfromHTML(InlineOutput);
+    setParsed(AST);
+  };
+
+  const Validity = () => {
+    setParsed("");
+    const AST = getASTfromHTML(InlineOutput);
+    console.log(AST);
+    const validity = ValidateTree(AST);
+    setValidity(validity);
+  };
 
   return (
     <DashboardLayout>
@@ -24,6 +50,14 @@ const Dashboard: NextPage = () => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className="p-4">
+        <button onClick={Parse}>Parse HTML to AST</button>
+        <button onClick={Validity}>Check Validity</button>
+        <br />
+        <br />
+        <p>{JSON.stringify(parsed)}</p>
+        <p>{JSON.stringify(validity)}</p>
+      </div>
     </DashboardLayout>
   );
 };
--- src/utils/editorHelpers.ts
+++ src/utils/editorHelpers.ts
@@ -1,23 +1,179 @@
-import { BaseEditor, Node } from "slate";
-import { ReactEditor, RenderElementProps } from "slate-react";
+import { unified } from "unified";
+import parse from "rehype-parse";
+import type { RootContent } from "hast";
 
-export const getDocument = (editor: BaseEditor & ReactEditor) =>
-  Array.from(Node.elements(editor));
-
-export const isCurrentElement = (
-  editor: BaseEditor & ReactEditor,
-  props: RenderElementProps
-) => getFocusedElement(editor) === props.element;
-
-export const getFocusedElement = (editor: BaseEditor & ReactEditor) => {
-  if (editor.selection?.anchor.path[0] !== undefined)
-    return editor.children[editor.selection?.anchor.path[0]];
-  else return undefined;
+export type NodeAST = ElementAST | TextAST;
+export type PureNodeAST = PureElementAST | PureTextAST;
+type PointAST = {
+  line: number;
+  column: number;
+  offset: number;
 };
 
-export const getFocusedProps = (editor: BaseEditor & ReactEditor) => {
-  return {
-    element: editor.children[editor.selection?.anchor.path[0]!],
-    anchor: editor.selection?.anchor,
+export interface ElementAST {
+  type: "element";
+  tagName: string;
+  properties: {
+    className?: string[];
   };
+  children: NodeAST[];
+  position: {
+    start: PointAST;
+    end: PointAST;
+  };
+}
+export interface PureElementAST {
+  type: "element";
+  tagName: string;
+  properties: {
+    className?: string[];
+  };
+  children: PureNodeAST[];
+}
+
+export interface TextAST {
+  type: "text";
+  value: string;
+  position: {
+    start: PointAST;
+    end: PointAST;
+  };
+}
+export interface PureTextAST {
+  type: "text";
+  value: string;
+}
+
+type ValidityError = "Invalid ClassName" | "Invalid TagName";
+
+export const getASTfromHTML = (html: string) => {
+  const processor = unified().use(parse, { fragment: true });
+
+  // there is a position property in each node of the AST
+  // which is not needed for our use case
+  const HtmlAST = processor.parse(html).children;
+  for (let index = 0; index < HtmlAST.length; index++) {
+    const node = HtmlAST[index];
+
+    deletePositionRecursive(node);
+  }
+
+  return HtmlAST as PureNodeAST[];
+  // example output
+  [
+    {
+      type: "element",
+      tagName: "p",
+      properties: {
+        className: ["ql-align-center"],
+      },
+      children: [[Object]],
+      position: {
+        start: { line: 1, column: 1, offset: 0 },
+        end: { line: 1, column: 27, offset: 26 },
+      },
+    },
+    {
+      type: "element",
+      tagName: "p",
+      properties: {},
+      children: [[Object]],
+    },
+    {
+      type: "element",
+      tagName: "p",
+      properties: {},
+      children: [[Object]],
+    },
+  ];
 };
+
+function deletePositionRecursive(node: any) {
+  if (node.position) {
+    delete node.position;
+  }
+  if (node.children) {
+    for (let i = 0; i < node.children.length; i++) {
+      deletePositionRecursive(node.children[i]);
+    }
+  }
+}
+
+export function ValidateTree(ast: PureNodeAST[]) {
+  let results: {
+    reason: ValidityError;
+    data: { tagName: string; className: string; expected: string };
+  }[] = [];
+  for (let index = 0; index < ast.length; index++) {
+    const node = ast[index];
+    console.log("called");
+    if (node?.type === "text") continue;
+
+    if (node?.type === "element") {
+      console.log(node);
+      const result = ValidateNode(node);
+      if (result) {
+        results.push(result);
+        console.log(results, "results");
+      }
+      if (node.children) {
+        results = [...results, ...ValidateTree(node.children)];
+      }
+    }
+  }
+
+  console.log(results, "results Final");
+  return results;
+}
+
+function ValidateNode(node: PureNodeAST):
+  | {
+      reason: ValidityError;
+      data: { tagName: string; className: string; expected: string };
+    }
+  | undefined {
+  // check if ast is valid
+  const validElementsMap = [
+    { type: "code", class: "inline-code" },
+    { type: "i", class: "a" },
+    { type: "mark", class: "cdx-marker" },
+  ];
+
+  const ValidElementsType = ["code", "i", "mark"];
+  const ValidElementsClass = ["inline-code", "a", "cdx-marker"];
+
+  if (node.type === "element") {
+    if (!ValidElementsType.includes(node.tagName)) {
+      return {
+        reason: "Invalid TagName",
+        data: {
+          tagName: node.tagName,
+          className: "",
+          expected: ValidElementsType.join(", "),
+        },
+      };
+    }
+
+    const tagNameIndex = ValidElementsType.indexOf(node.tagName);
+    if (
+      ValidElementsClass[tagNameIndex] !==
+      (node.properties.className !== undefined
+        ? node.properties.className.join("")
+        : "")
+    ) {
+      return {
+        reason: "Invalid ClassName",
+        data: {
+          className:
+            node.properties.className !== undefined &&
+            node.properties.className[0] !== undefined
+              ? node.properties.className[0]
+              : " ",
+          tagName: "",
+          expected: ValidElementsClass[tagNameIndex] || " ",
+        },
+      };
+    }
+  }
+  return undefined;
+}
