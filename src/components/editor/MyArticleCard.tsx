e082b21bc770cfa7f2ec025850ac8c780f820c41
Bekogeko <gulestanbekir@gmail.com>
2022-12-20 05:59:43 +0300

    Add Create Article Functionality
    Add isPublished Data for MyArticleCard 
    Now article can be created within a dialog inside profile page.
    
    The created articles will be set to isPublished:false to edit and publish users should be directed to /articles/:slug/edit
    
    in the edit page they will be a publish/update button depending on the state.

--- src/components/editor/MyArticleCard.tsx
+++ src/components/editor/MyArticleCard.tsx
@@ -4,18 +4,44 @@ interface MyArticleProps {
   slug: string;
   title: string;
   description: string;
+  isPublished: boolean;
 }
 
 const MyArticleCard: NextPage<MyArticleProps> = ({
   title,
   description,
   slug,
+  isPublished,
 }) => {
   return (
-    <Link href={`/articles/${slug}`}>
-      <div className="bg-gray-100 col-span-1 group flex flex-col items-start justify-start p-4 rounded-md shadow-sm hover:shadow-md hover:bg-indigo-500 min-h-[128px]">
-        <p className="text-lg font-medium group-hover:text-white ">{title}</p>
-        <p className="line-clamp-3 group-hover:text-white"> {description}</p>
+    <Link href={!isPublished ? `/articles/${slug}/edit` : `/articles/${slug}`}>
+      <div
+        className={`bg-gray-100 col-span-1 group flex flex-col items-start justify-start
+                       p-4 rounded-md shadow-sm hover:shadow-md min-h-[128px]
+                       ${
+                         !isPublished
+                           ? "border-2 border-dashed bg-white border-gray-300 hover:bg-gray-100 hover:border-indigo-500"
+                           : "hover:bg-indigo-500"
+                       }`}
+      >
+        <p
+          className={`text-lg font-medium  ${
+            !isPublished
+              ? "group-hover:text-indigo-500"
+              : "group-hover:text-white"
+          } `}
+        >
+          {title}
+        </p>
+        <p
+          className={`line-clamp-3  ${
+            !isPublished
+              ? "group-hover:text-indigo-500"
+              : "group-hover:text-white"
+          }`}
+        >
+          {description}
+        </p>
       </div>
     </Link>
   );
--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -5,24 +5,56 @@ import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
 
+import { useState } from "react";
 import { useSession } from "next-auth/react";
 
 import DashboardLayout from "../components/layouts/dashboard";
-import DragScrollContainer from "../components/DragScrollContainer";
-import ArticleCard from "../components/ArticleCard";
-import { PlusIcon } from "@heroicons/react/24/outline";
 import MyArticleCard from "../components/editor/MyArticleCard";
+
+import { PlusIcon } from "@heroicons/react/24/outline";
+import { Dialog } from "@headlessui/react";
+import Loading from "../components/Loading";
 
 const Articles: NextPage = () => {
   const { data } = useSession({ required: true });
   const user = data?.user;
 
+  const [isOpen, setIsOpen] = useState(false);
+  const [dialogTitle, setDialogTitle] = useState("");
+  const [dialogDescription, setDialogDescription] = useState("");
+
   const articleData = trpc.useQuery([
     "article.getUserArticleInfos",
     { userId: user?.id },
   ]);
+  const createQuery = trpc.useQuery(
+    [
+      "article.createArticle",
+      { title: dialogTitle, description: dialogDescription, bodyData: [] },
+    ],
+    {
+      enabled: false,
+    }
+  );
 
-  console.log(articleData);
+  const onSubmitDialog = () => {
+    createQuery
+      .refetch()
+      .then(() => {
+        // refresh "My articles" data
+        articleData.refetch();
+
+        setDialogDescription("");
+        setDialogTitle("");
+        setIsOpen(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    // console.log(createQuery.data);
+  };
+
   return (
     <DashboardLayout>
       <Head>
@@ -42,19 +74,93 @@ const Articles: NextPage = () => {
               title={article.title}
               description={article.description}
               slug={article.slug}
+              isPublished={article.isPublished}
               // image={article.image}
             />
           ))}
 
           {/* Add Project div */}
-          <Link href="/articles/new">
-            <button className="col-span-1 group bg-white border-dashed border-2 rounded-md hover:border-solid hover:border-indigo-500 flex flex-col justify-center items-center">
-              <PlusIcon className="w-6 h-6 group-hover:text-indigo-500"></PlusIcon>
-              <p className="group-hover:text-indigo-500 text-sm leading-6 font-medium">
-                Create New Article
-              </p>
-            </button>
-          </Link>
+          <button
+            className="col-span-1 group bg-white border-dashed border-2 rounded-md hover:border-solid hover:border-indigo-500 flex flex-col justify-center items-center"
+            onClick={() => setIsOpen(true)}
+          >
+            <PlusIcon className="w-6 h-6 group-hover:text-indigo-500"></PlusIcon>
+            <p className="group-hover:text-indigo-500 text-sm leading-6 font-medium">
+              Create New Article
+            </p>
+          </button>
+
+          {/* Add Stepper dialog for Tag and topic selection */}
+          <Dialog
+            open={isOpen}
+            onClose={() => {
+              setDialogDescription("");
+              setDialogTitle("");
+              setIsOpen(false);
+            }}
+          >
+            <div
+              className="fixed inset-0 bg-black/30  backdrop-blur-md"
+              onClick={(e) => {
+                e.stopPropagation();
+                e.preventDefault();
+              }}
+            />
+            <div className="fixed inset-0 flex items-center justify-center  ">
+              <Dialog.Panel className="bg-white p-4 rounded-2xl w-2/5">
+                <Dialog.Title className="text-xl font-bold">
+                  Create new article
+                </Dialog.Title>
+                <Dialog.Description className={"text-sm font-light"}>
+                  Enter a name and a description for your new article.
+                </Dialog.Description>
+                <label htmlFor="articleName" className="mt-4 block">
+                  Title
+                </label>
+                <input
+                  name="articleName"
+                  type="text"
+                  className="w-full border border-gray-200 rounded-lg p-2 "
+                  value={dialogTitle}
+                  onChange={(e) => setDialogTitle(e.target.value)}
+                />
+
+                <label htmlFor="articleDescription" className="mt-4 block">
+                  Description
+                </label>
+                <textarea
+                  name="articleDescription"
+                  className="w-full border border-gray-200 rounded-lg p-2"
+                  value={dialogDescription}
+                  onChange={(e) => setDialogDescription(e.target.value)}
+                ></textarea>
+                <div className="flex flex-row justify-between">
+                  <button
+                    className="mr-auto"
+                    onClick={() => {
+                      setDialogDescription("");
+                      setDialogTitle("");
+                      setIsOpen(false);
+                    }}
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    className="flex flex-row gap-1 justify-center items-center p-2 bg-indigo-500 hover:bg-indigo-400 rounded-md text-white text-sm font-medium disabled:bg-indigo-300"
+                    onClick={() => onSubmitDialog()}
+                    disabled={createQuery.isLoading}
+                  >
+                    {createQuery.isLoading ? (
+                      <Loading className="w-6 h-6 border-4" />
+                    ) : (
+                      ""
+                    )}
+                    Continue
+                  </button>
+                </div>
+              </Dialog.Panel>
+            </div>
+          </Dialog>
         </div>
       </div>
     </DashboardLayout>
