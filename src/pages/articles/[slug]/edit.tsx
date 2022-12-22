0fd861fe7f7ff595bad4d7b67f9ed45bee749270
Deniz <hincal32619@yazilimlisesi.k12.tr>
2022-12-22 04:02:01 +0300

    Add Save Func. to Slug Edit Page
    - Save functionality added to the edit page with EditorTopBar and the newer version of the editor is used for getting the blocks data in the editor.

--- src/pages/articles/[slug]/edit.tsx
+++ src/pages/articles/[slug]/edit.tsx
@@ -1,16 +1,18 @@
 import type { NextPage } from "next";
+import EditorJS from "@editorjs/editorjs";
 
 import { trpc } from "../../../utils/trpc";
 import Head from "next/head";
 
 import ArticleError from "../../../components/responses/ArticleError";
 import Loading from "../../../components/Loading";
+import EditorTopbar from "../../../components/editor/EditorTopbar";
 import DashboardLayout from "../../../components/layouts/dashboard";
 
 // load editor only on client side
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 import dynamic from "next/dynamic";
 const Editor = dynamic(() => import("../../../components/Editor"), {
@@ -20,9 +22,15 @@ const Editor = dynamic(() => import("../../../components/Editor"), {
 const Articles: NextPage = ({}) => {
   const { status } = useSession({ required: true });
   const [isFetching, setIsFetching] = useState(true);
-
+  const [bodyData, setBodyData] = useState<any>(null);
   const router = useRouter();
   const { slug } = router.query;
+
+  const [editor, setEditor] = useState<EditorJS | null>(null);
+
+  const handleInit = useCallback((instance: EditorJS) => {
+    setEditor(instance);
+  }, []);
 
   const articleAuthorFetch = trpc.useQuery(
     ["article.getArticleBySlugAuthor", { slug: slug as string }],
@@ -35,28 +43,61 @@ const Articles: NextPage = ({}) => {
       //   refetchIntervalInBackground: false,
     }
   );
+  const articleUpdateBodyFetch = trpc.useQuery(
+    [
+      "article.updateArticleBody",
+      {
+        slug: slug as string,
+        bodyData,
+      },
+    ],
+    {
+      enabled: false,
+    }
+  );
 
   useEffect(() => {
     if (articleAuthorFetch.isSuccess && status === "authenticated")
       setIsFetching(false);
   }, [status, articleAuthorFetch.isSuccess]);
 
+  useEffect(() => {
+    if (bodyData) {
+      articleUpdateBodyFetch.refetch();
+    }
+  }, [bodyData]);
+
+  const OnSave = () => {
+    editor?.save().then((outputData) => {
+      console.log(outputData);
+      setBodyData(outputData.blocks);
+    });
+    // console.log(editor);
+  };
   return (
     <DashboardLayout>
       <Head>
         <title>Edit Mode - Pulth App</title>
       </Head>
-      <div className="p-4">
+      <div>
         {!isFetching ? (
           articleAuthorFetch.data ? (
-            <Editor
-              readonly={false}
-              data={{
-                time: articleAuthorFetch.data.updatedAt,
-                blocks: articleAuthorFetch.data.bodyData,
-                version: articleAuthorFetch.data.editorVersion,
-              }}
-            />
+            <div>
+              <EditorTopbar
+                slug={slug as string}
+                onSave={OnSave}
+                saveLoading={articleUpdateBodyFetch.isLoading}
+              />
+              <Editor
+                readonly={false}
+                data={{
+                  time: articleAuthorFetch.data.updatedAt,
+                  blocks: articleAuthorFetch.data.bodyData,
+                  version: articleAuthorFetch.data.editorVersion,
+                }}
+                OnInit={handleInit}
+              />
+            </div>
           ) : (
             <ArticleError
               title="You are not the author of this file"
