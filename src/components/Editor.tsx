a7f1e992baf1de0421e5f4d1c39933eb2eaaaf22
Deniz <hincal32619@yazilimlisesi.k12.tr>
2022-12-22 03:53:00 +0300

    Rework Editor Component
    - Editor Component now gives a callback for `OnInit` and we are using it for getting our editor Instance in Parent Component
    - Stability is still unknown

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -7,7 +7,75 @@ const Editor: NextPage<{
   className?: string;
   data?: any;
   readonly: boolean;
-}> = ({ className, data, readonly }) => {
+  OnInit?: (instance: EditorJS) => void;
+}> = ({ className, data, readonly, OnInit }) => {
+  const id = useId();
+
+  const [editor, setEditor] = useState<EditorJS | null>(null);
+  useEffect(() => {
+    setEditor((prevEditor) => {
+      if (!prevEditor) {
+        const newEditor = new EditorJS({
+          holder: id,
+          autofocus: false,
+          data: data,
+          readOnly: readonly,
+          onReady: () => OnInit(newEditor),
+          tools: {
+            header: {
+              class: require("@editorjs/header"),
+              inlineToolbar: ["link"],
+              config: {
+                placeholder: "Header",
+              },
+            },
+            list: {
+              class: require("@editorjs/list"),
+              inlineToolbar: true,
+              shortcut: "CMD+SHIFT+L",
+            },
+            // todo Add more tools
+            // such as
+            // quote: require("@editorjs/quote"),
+            // warning: require("@editorjs/warning"),
+            // marker: require("@editorjs/marker"),
+            // code: require("@editorjs/code"),
+            // delimiter: require("@editorjs/delimiter"),
+            // inlineCode: require("@editorjs/inline-code"),
+            // linkTool: require("@editorjs/link"),
+            // raw: require("@editorjs/raw"),
+            // checklist: require("@editorjs/checklist"),
+
+            // Todo add image upload Endpoints etc.
+            // image: {
+            //   class: require("@editorjs/image"),
+            //   config: {
+            //     endpoints: {
+            //       byFile: "http://localhost:8008/uploadFile", // Your backend file uploader endpoint
+            //       byUrl: "http://localhost:8008/fetchUrl", // Your endpoint that provides uploading by Url
+            //     },
+            //   },
+            // },
+          },
+        });
+        return newEditor;
+      }
+      return null;
+    });
+    return () => {
+      if (editor) editor.destroy();
+    };
+    // solve exaustive deps warning
+  }, [id]);
+
+  return <div id={id} className={className + " "}></div>;
+};
+
+export default Editor;
+
+/*
+
+
   const id = useId();
   const [editor, setEditor] = useState<EditorJS | null>(null);
 
@@ -68,14 +136,13 @@ const Editor: NextPage<{
     return () => {
       // destroy editor if it's set
       if (editor) {
+        editorRef.current = null;
         editor.destroy();
       }
     };
 
     // run effect only if id changes
-  }, [editor, id]);
+  }, [id]);
 
   return <div id={id} className={className + ""}></div>;
-};
-
-export default Editor;
+*/
