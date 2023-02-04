3f64369586ff512ce985e32271ef7b75f5b49580
Bekogeko <gulestanbekir@gmail.com>
2023-02-05 01:42:12 +0300

    Create Basic Comment System
    - Comment router added to tRPC appRouter.
    - AddComment ui abstracted to component. Component uses a native js function to determine the height of the textarea.
    - Added authorId to articles comments to identify is the user is the author of this comment.
    - Added a router to Add, delete, and update the comment. Although we cant really delete the comments because of a limitation with prisma and MongoDB ( #9 )
    - Refactor slugIndex.
    - CommentList Component now creates a tree data structure to render Comments

--- src/components/editor/comment.tsx
+++ src/components/editor/comment.tsx
@@ -2,97 +2,82 @@ import {
   ArrowUturnLeftIcon,
   ChevronDownIcon,
   ChevronUpIcon,
+  PencilSquareIcon,
+  TrashIcon,
 } from "@heroicons/react/24/outline";
 import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useState } from "react";
-import { trpc } from "../../utils/trpc";
 
 import Image from "next/image";
+import CommentAdd, { AddCommentData } from "./addComment";
+import { trpc } from "../../utils/trpc";
 
-interface Comment {
+interface CommentData {
+  // id of the comment
   id: string;
+  // id of the parent comment
+  // null if this is a top level comment
+  parentId: string | null;
+
+  // author's data of the comment
   author: {
+    id: string;
     image: string | null;
     name: string | null;
   };
+
+  // content of the comment
   content: string;
+  // rating of the comment
   rating: number;
-  parentId: string | null;
-}
-interface CommentProps {
-  comment: Comment;
-  subComments: Comment[];
-  allComments: Comment[];
-  commentsByParentId: {
-    [key: string]: {
-      comments: Comment[];
-      childrenIds: string[];
-      depth: number;
-    };
-  };
-  OnClick: (comment: Comment) => void;
+
+  // sub comments of this comment
+  // null if this comment has no sub comments
+  subComments?: CommentData[];
 }
 
+//
+//  WARNING: Currently the "rating" of the comment is not implemented
+//           and is always 0. This is because the rating system is not implemented yet.
+//            so beacuse of this the "rating" is not shown in the UI.
+//
 const Comment = ({
   comment,
-  subComments,
-  allComments,
-  commentsByParentId,
-  OnClick,
-}: CommentProps) => {
-  const { data, status } = useSession();
-  const user = data?.user;
+  currentUser,
+  currentArticleId,
+  commentDepth,
+  OnAnyEdit,
+}: {
+  // comment is the data of the comment
+  comment: CommentData;
+  // How nested this comment is
+  // this is used to prevent to much nesting.
+  // this is automatically calculated while rendering the comments recursively
+  commentDepth: number;
 
-  const requestedSubComments = 2;
-  let shouldShown: boolean = false;
+  // currentUser is the data of the current user
+  // used to check if the current user is the author of the comment
+  currentUser: { id: string; name: string; image: string };
 
-  const [replyShow, setReplyShow] = useState(false);
-  const [replyContent, setReplyContent] = useState("");
+  // used to create a new comment in a mutation
+  currentArticleId: string;
 
-  const router = useRouter();
-  const slug = router.query.slug as string;
+  // OnAnyEdit is a callback function that is called when this comment or its children are edited
+  OnAnyEdit: () => void;
+}) => {
+  const [showReply, setShowReply] = useState(false);
+  const [editValue, setEditValue] = useState("");
+  const [showEdit, setShowEdit] = useState(false);
 
-  const createCommentQuery = trpc.useQuery(
-    [
-      "article.createComment",
-      {
-        slug,
-        content: replyContent,
-        parentId: comment.parentId ?? undefined,
-      },
-    ],
-    {
-      enabled: false,
-    }
-  );
-
-  const handleReply = () => {
-    setReplyShow(!replyShow);
-    createCommentQuery.refetch();
-  };
-
-  shouldShown =
-    !comment.parentId ||
-    (commentsByParentId[comment.id] &&
-      commentsByParentId[comment.id]?.depth! < requestedSubComments)
-      ? true
-      : false;
+  const commentAddMutation = trpc.useMutation("comment.addComment");
+  const commentUpdateMutation = trpc.useMutation("comment.updateComment");
+  const commentDeleteMutation = trpc.useMutation("comment.deleteComment");
 
   return (
-    <div className="">
-      <div className="flex items-center relative">
-        {/* Rating count and arrows beside that*/}
-        <div className="flex flex-col self-stretch mr-2 items-center absolute -translate-x-8 -translate-y-3">
-          <button>
-            <ChevronUpIcon className="h-5 self-stretch aspect-square stroke-black/60 hover:stroke-black/100" />
-          </button>
-          <p className="leading-3 text-sm">{comment.rating} </p>
-          <button>
-            <ChevronDownIcon className="h-5 self-stretch aspect-square stroke-black/60 hover:stroke-black/100" />
-          </button>
-        </div>
-        <div className="relative w-8 h-8">
+    <div>
+      <div className="flex gap-2">
+        <div className="relative w-8 h-8 flex-shrink-0">
           <Image
             alt="avatar"
             layout="fill"
@@ -100,97 +85,115 @@ const Comment = ({
             className="w-8 h-8 rounded-full"
           />
         </div>
-        <p className="ml-2  font-medium ">{comment.author.name}</p>
-        {shouldShown ? (
-          <button
-            className="ml-auto font-normal text-sm flex gap-x-2 items-center"
-            onClick={() => {
-              setReplyShow(!replyShow);
-            }}
-          >
-            Reply
-            <ArrowUturnLeftIcon className="h-4" />
-          </button>
-        ) : (
-          ""
-        )}
-        {/* Todo add comment date */}
-      </div>
-      <p className={` ${replyShow ? "" : " mb-4 "} ml-10`}>{comment.content}</p>
-      {replyShow ? (
-        <div className="ml-10 mb-4 mt-2 flex items-center gap-x-2">
-          <div className="relative w-8 h-8 flex-shrink-0 self-start">
-            <Image
-              alt="avatar"
-              layout="fill"
-              src={user?.image || "/default_profile.jpg"}
-              className="w-8 h-8 rounded-full"
-            />
-          </div>
-          {status === "unauthenticated" ? (
-            <button
-              className="w-full  bg-gray-700 flex items-center justify-center py-2 rounded-md"
-              onClick={() => signIn()}
-            >
-              <p className="text-gray-200">Login to reply</p>
-            </button>
-          ) : (
-            <div className="flex flex-col flex-grow">
+        <div className={`${showEdit ? "w-full" : "mr-auto"} `}>
+          <p className="font-semibold leading-5">{comment.author.name}</p>
+          {showEdit ? (
+            <>
               <textarea
-                className=" border p-1 rounded-md"
-                placeholder="Add Comment..."
-                onChange={(e) => {
-                  setReplyContent(e.target.value);
-                }}
-              ></textarea>
-              <div className="flex gap-x-2 ml-auto my-2">
+                className="bg-[#fafafa] rounded-md outline-gray-300 w-full px-2 py-1 resize-none border-2 border-gray-200 overflow-y-hidden"
+                value={editValue}
+                onChange={(e) => setEditValue(e.target.value)}
+              />
+              <div className="flex justify-end gap-2">
                 <button
-                  className="hover:bg-black/10 p-2  rounded-md"
-                  onClick={() => {
-                    setReplyShow(false);
+                  onClick={async () => {
+                    // TODO: Add edit comment mutation
+                    await commentUpdateMutation.mutate({
+                      id: comment.id,
+                      content: editValue,
+                    });
+                    OnAnyEdit();
+                    setShowEdit(!showEdit);
                   }}
+                  className="p-2 bg-indigo-500 hover:bg-indigo-400 active:bg-indigo-600 rounded-md text-white flex gap-2 items-center disabled:bg-indigo-400"
+                  disabled={editValue === "" || editValue === comment.content}
+                >
+                  Save
+                </button>
+                <button
+                  onClick={() => {
+                    setEditValue(comment.content);
+                    setShowEdit(!showEdit);
+                  }}
+                  className="p-2 bg-gray-200 hover:bg-gray-300 active:bg-gray-400 rounded-md"
                 >
                   Cancel
                 </button>
-                <button
-                  className="p-2 bg-indigo-500 text-white rounded-md hover:bg-indigo-400"
-                  onClick={() => {
-                    handleReply();
-                  }}
-                >
-                  Comment
-                </button>
               </div>
-            </div>
+            </>
+          ) : (
+            <p className="">{comment.content}</p>
           )}
         </div>
-      ) : (
-        ""
-      )}
-      <div className="ml-10">
-        {shouldShown
-          ? subComments?.map((comment) => (
-              <Comment
-                comment={comment}
-                allComments={allComments}
-                commentsByParentId={commentsByParentId}
-                subComments={
-                  commentsByParentId && commentsByParentId[comment.id]
-                    ? allComments.filter((childComment) =>
-                        commentsByParentId[comment.id]!.childrenIds.includes(
-                          childComment.id
-                        )
-                      )
-                    : []
-                }
-                OnClick={OnClick}
-                key={comment.id}
-              />
-            ))
-          : null}
+        {currentUser.id === comment.author.id ? (
+          <>
+            <button
+              onClick={() => {
+                setEditValue(comment.content);
+                setShowEdit(!showEdit);
+              }}
+            >
+              <PencilSquareIcon className="w-5 h-5" />
+            </button>
+            <button
+              onClick={() => {
+                commentDeleteMutation.mutate({ id: comment.id });
+              }}
+            >
+              <TrashIcon className="w-5 h-5" />
+            </button>
+          </>
+        ) : (
+          commentDepth < 3 && (
+            <button
+              onClick={() => {
+                if (commentDepth < 3) setShowReply(!showReply);
+              }}
+            >
+              <ArrowUturnLeftIcon className="w-5 h-5" />
+            </button>
+          )
+        )}
+      </div>
+      <div className="pl-10 mt-2 ">
+        {/* if showReply is true (if we clicked to reply button) show the comment add Comp. */}
+        {showReply ? (
+          <CommentAdd
+            className=""
+            parentId={comment.id}
+            user={{ name: currentUser.name, image: currentUser.image }}
+            isLoading={false}
+            OnComment={(e: AddCommentData) => {
+              commentAddMutation.mutate({
+                content: e.content,
+                parentId: e.parent,
+                articleId: currentArticleId,
+              });
+              setShowReply(false);
+            }}
+            OnCancel={() => {
+              setShowReply(false);
+            }}
+          />
+        ) : (
+          ""
+        )}
+        {comment.subComments?.map((subComment) => {
+          return (
+            <Comment
+              key={subComment.id}
+              currentUser={currentUser}
+              comment={subComment}
+              currentArticleId={currentArticleId}
+              commentDepth={commentDepth + 1}
+              OnAnyEdit={OnAnyEdit}
+            />
+          );
+        })}
       </div>
     </div>
   );
 };
 
 export default Comment;
+export type { CommentData };
