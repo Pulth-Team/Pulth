369e13d5f81c1077247bbdd4ae0d84f6258351dc
Deniz <hincal32619@yazilimlisesi.k12.tr>
2023-02-04 19:14:55 +0300

    Fix Auth Vulnerability of Settings
    We fixed the vulnerability of two mistakes:
     - Query code has been moved to the bottom of the middleware to prevent the actions of settings for people who didn't login.
     - Id that used for update process now changed to id of user in session.

--- src/server/router/auth.ts
+++ src/server/router/auth.ts
@@ -8,34 +8,7 @@ export const authRouter = createRouter()
       return ctx.session;
     },
   })
-  .query("updateSettings",{
-    input:z.object({
-      "userId":z.string(),
 
-      "data": z.object({
-        "name":z.string().optional(),
-
-        // todo Add Photo Change
-        // "photo": z.string().optional()
-
-        // Todo Add change Email
-        // "email": z.string().optional()
-      })
-    }),
-    async resolve({input}){
-      
-      const updatedUser  = await prisma?.user.update({
-        where:{
-        id: input.userId
-        },
-        data:{
-          name: input.data.name ?? undefined
-        }
-      })
-      
-      return updatedUser ? "Updated" : "Something went wrong while updating"
-    }
-  })
   .middleware(async ({ ctx, next }) => {
     // Any queries or mutations after this middleware will
     // raise an error unless there is a current session
@@ -43,6 +16,35 @@ export const authRouter = createRouter()
       throw new TRPCError({ code: "UNAUTHORIZED" });
     }
     return next();
+  })
+
+  .query("updateSettings", {
+    input: z.object({
+      userId: z.string(),
+
+      data: z.object({
+        name: z.string().optional(),
+        description: z.string().optional(),
+        // todo Add Photo Change
+        // "photo": z.string().optional()
+
+        // Todo Add change Email
+        // "email": z.string().optional()
+      }),
+    }),
+    async resolve({ input, ctx }) {
+      const updatedUser = await ctx.prisma?.user.update({
+        where: {
+          id: ctx.session?.user?.id,
+        },
+        data: {
+          name: input.data.name ?? undefined,
+          description: input.data.description ?? undefined,
+        },
+      });
+
+      return updatedUser ? "Updated" : "Something went wrong while updating";
+    },
   })
   .query("getSecretMessage", {
     async resolve({ ctx }) {
