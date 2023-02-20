9124736a7efde7465473b2ece0a690866c73a11c
Bekogeko <gulestanbekir@gmail.com>
2023-02-20 06:54:09 +0300

    Add Align System to Tour Component
    + Alignment added to each tour with 3 option ("start", "center","end"). Start and end options is determined by the x axis the left side is always remembered as start option.
    + Alignment removed from switch and moved to its own function in the future we might want to merge these two functions together (calculateLeft,calculateTop can be merged into calculatePosition etc.)
    + `onMobile`field removed from SearchButton Component
    + Id added top SearchButton component in oreder to work with TourComponent
    + `Search` text converted to `sr-only` for improving SEO
    + Add undefined check for classname insade SearchButton Component.

--- src/components/Tour.tsx
+++ src/components/Tour.tsx
@@ -1,9 +1,72 @@
 // imports GetServerSideProps from nextjs
 import { NextPage } from "next";
 import Link from "next/link";
-import React, { Children, useMemo, useRef, useState } from "react";
-import { text } from "stream/consumers";
+import React, {
+  Children,
+  useMemo,
+  useRef,
+  useState,
+  useLayoutEffect,
+} from "react";
 import { BatchElement } from "../types/renderer";
+
+function isHidden(el: HTMLElement) {
+  //   var style = window.getComputedStyle(el);
+  //   return style.display === "none";
+  return el.offsetParent === null;
+}
+
+function calculatePositionLeft(
+  targetPosition: DOMRect,
+  direction: "top" | "bottom" | "left" | "right",
+  align: "start" | "center" | "end",
+  tourWidth: number
+) {
+  if (direction === "bottom" || direction === "top") {
+    switch (align) {
+      case "start":
+        return targetPosition.left;
+      case "center":
+        return targetPosition.left - tourWidth / 2 + targetPosition.width / 2;
+      case "end":
+        return targetPosition.left - tourWidth + targetPosition.width;
+    }
+  }
+
+  if (direction === "left") {
+    return targetPosition.left - tourWidth;
+  }
+
+  if (direction === "right") {
+    return targetPosition.left + targetPosition.width;
+  }
+}
+
+function calculatePositionTop(
+  targetPosition: DOMRect,
+  direction: "top" | "bottom" | "left" | "right",
+  align: "start" | "center" | "end",
+  tourHeight: number
+) {
+  if (direction === "left" || direction === "right") {
+    switch (align) {
+      case "start":
+        return targetPosition.top;
+      case "center":
+        return targetPosition.top - tourHeight / 2 + targetPosition.height / 2;
+      case "end":
+        return targetPosition.top - tourHeight + targetPosition.height;
+    }
+  }
+
+  if (direction === "top") {
+    return targetPosition.top - tourHeight;
+  }
+
+  if (direction === "bottom") {
+    return targetPosition.top + targetPosition.height;
+  }
+}
 
 // creates a NextFunctionComponent
 const Tour: NextPage<{
@@ -13,9 +76,10 @@ const Tour: NextPage<{
 
   targetQueries: string[];
   directions: ("top" | "bottom" | "left" | "right")[];
+  aligns: ("start" | "center" | "end")[];
   messages: string[];
 
-  onFinished?: (e: "success" | "backdrop" | "skipped") => void;
+  onFinished: (e: "success" | "backdrop" | "skipped" | "error") => void;
 }> = ({
   start,
 
@@ -24,6 +88,7 @@ const Tour: NextPage<{
 
   targetQueries,
   directions,
+  aligns,
   messages,
 
   onFinished,
@@ -32,50 +97,82 @@ const Tour: NextPage<{
   const [lastTarget, setLastTarget] = useState<HTMLElement | null>(null);
   const [tourIndex, setTourIndex] = useState(0);
 
-  const clearTour = (e: "success" | "backdrop" | "skipped") => {
+  const clearTour = (e: "success" | "backdrop" | "skipped" | "error") => {
     lastTarget?.style.setProperty("z-index", "auto");
     setTourIndex(0);
     setLastTarget(null);
-    onFinished?.(e);
+    onFinished(e);
   };
 
-  // checks if the length of the targetQueries, directions and messages are equal
+  // checks if the length of the targetQueries, directions, messages and aligns are equal
   if (
     targetQueries.length !== directions.length ||
-    directions.length !== messages.length
+    directions.length !== messages.length ||
+    messages.length !== aligns.length
   ) {
-    console.error("targetQueries, directions and messages are not equal");
+    console.error(
+      "targetQueries, directions, messages and aligns must have the same length"
+    );
+
     return null;
   }
 
-  useMemo(() => {
-    if (!start) return null;
+  useLayoutEffect(() => {
+    if (!start) return clearTour("error");
 
     // gets the first query and direction
     const currentQuery = targetQueries[tourIndex];
     const currentDirection = directions[tourIndex];
+    const currentAlign = aligns[tourIndex];
 
-    if (!currentQuery || !currentDirection) {
-      console.error("Query or direction are null");
+    if (!currentQuery || !currentDirection || !currentAlign) {
+      console.error("Query, direction or Align are null");
 
-      clearTour("success");
-      return null;
+      return clearTour("error");
     }
 
     // gets the target element
-    const target = document.querySelector<HTMLElement>(currentQuery);
+    const targetAll = document.querySelectorAll<HTMLElement>(currentQuery);
 
-    // sets the last target
-    setLastTarget(target);
+    // checks if the target is null
+    if (!targetAll) {
+      console.error("target is null");
+
+      return clearTour("error");
+    }
+
+    let visibleTarget: HTMLElement | null = null;
+    if (targetAll.length > 1) {
+      // checks if the target is an array
+
+      const targetArray = Array.from(targetAll);
+      console.log("target is an array calculating the first visible element");
+
+      // loops through the array
+      const filteredVisibleTargets = targetArray.filter((target) => {
+        return !isHidden(target);
+      });
+
+      if (filteredVisibleTargets.length > 0)
+        visibleTarget =
+          filteredVisibleTargets[0] === undefined
+            ? null
+            : filteredVisibleTargets[0];
+    }
+    const target = visibleTarget || targetAll[0];
+
+    if (!target) {
+      console.error("target is null");
+
+      return clearTour("error");
+    }
 
     // calculates the position of the target element
     const targetPosition = target?.getBoundingClientRect();
 
-    console.log(targetPosition);
-
     if (!targetPosition) {
       console.error("target position is null");
-      return null;
+      return clearTour("error");
     }
 
     // calculates tours height and width
@@ -86,55 +183,28 @@ const Tour: NextPage<{
     if (typeof tourHeight !== "number" || typeof tourWidth !== "number") {
       console.error("tour height or width is null", tourHeight, tourWidth);
 
-      return null;
+      return clearTour("error");
     }
 
-    // switch case for the direction of the tour
-    switch (currentDirection) {
-      case "top":
-        tourRef.current?.style.setProperty(
-          "top",
-          `${targetPosition?.top + window.scrollY - tourHeight}px`
-        );
-        tourRef.current?.style.setProperty(
-          "left",
-          `${targetPosition?.left + window.scrollX}px`
-        );
-        break;
-      case "bottom":
-        tourRef.current?.style.setProperty(
-          "top",
-          `${targetPosition?.top + window.scrollY + targetPosition?.height}px`
-        );
-        tourRef.current?.style.setProperty(
-          "left",
-          `${targetPosition?.left + window.scrollX}px`
-        );
-        break;
-      case "left":
-        tourRef.current?.style.setProperty(
-          "top",
-          `${targetPosition?.top + window.scrollY}px`
-        );
-        tourRef.current?.style.setProperty(
-          "left",
-          `${targetPosition?.left + window.scrollX - tourWidth}px`
-        );
-        break;
-      case "right":
-        tourRef.current?.style.setProperty(
-          "top",
-          `${targetPosition?.top + window.scrollY}px`
-        );
-        tourRef.current?.style.setProperty(
-          "left",
-          `${targetPosition?.left + window.scrollX + targetPosition?.width}px`
-        );
-        break;
-      default:
-        console.error("direction is not valid");
-        break;
-    }
+    tourRef.current?.style.setProperty(
+      "left",
+      `${calculatePositionLeft(
+        targetPosition,
+        currentDirection,
+        currentAlign,
+        tourWidth
+      )}px`
+    );
+
+    tourRef.current?.style.setProperty(
+      "top",
+      `${calculatePositionTop(
+        targetPosition,
+        currentDirection,
+        currentAlign,
+        tourHeight
+      )}px`
+    );
 
     // desets the z-index of the last target
     lastTarget?.style.setProperty("z-index", "auto");
@@ -142,6 +212,14 @@ const Tour: NextPage<{
     // sets the z-index of the tour element
     tourRef.current?.style.setProperty("z-index", "50");
     target?.style.setProperty("z-index", "50");
+
+    // sets the last target
+    setLastTarget(target);
+
+    // checks if the tour index is equal to the length of the target queries
+    if (tourIndex === targetQueries.length) {
+      clearTour("success");
+    }
   }, [tourIndex, start]);
 
   return (
