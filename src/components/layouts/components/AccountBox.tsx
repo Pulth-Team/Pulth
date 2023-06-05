a6727db64f81f32557bd377ca2fd8ce19d11b9d4
Bekogeko <gulestanbekir@gmail.com>
2023-06-05 05:43:29 +0300

    GridDashboard split into components
    Accountboxes splitted.

---
+++ src/components/layouts/components/AccountBox.tsx
@@ -0,0 +1,108 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import { Menu } from "@headlessui/react";
+import {
+  ArrowLeftOnRectangleIcon,
+  Cog8ToothIcon,
+  UserCircleIcon,
+} from "@heroicons/react/24/outline";
+
+
+import { twMerge } from "tailwind-merge";
+import { signOut } from "next-auth/react";
+
+const AccountBox = ({
+  image,
+  name,
+  path,
+  id,
+  className,
+}: {
+  image: string;
+  name: string;
+  path: string;
+  id: string;
+  className?: string;
+}) => {
+  return (
+    <div
+      className={twMerge(
+        "flex",
+        className,
+        "mt-auto rounded-md bg-gray-700 p-2 "
+      )}
+      id="current-account-box"
+    >
+      <Menu>
+        <Menu.Button className="relative h-12 w-12 overflow-hidden rounded-full">
+          <Image
+            src={image || "/default_profile.jpg"}
+            layout="fill"
+            className="rounded-full"
+            alt="Profile Picture"
+          />
+        </Menu.Button>
+        <Menu.Items
+          className={`absolute bottom-0 -translate-y-20 rounded-md bg-gray-700 p-1 `}
+        >
+          <Menu.Item>
+            <div>
+              <Link
+                href="/profile"
+                className={`${
+                  path === "/profile" ? "bg-gray-600" : "bg-gray-700"
+                } flex cursor-pointer flex-row items-center gap-2 rounded-md p-2  text-gray-100`}
+              >
+                <UserCircleIcon className="h-6 w-6" />
+                <p>Profile</p>
+              </Link>
+            </div>
+          </Menu.Item>
+          <Menu.Item>
+            <div>
+              <Link
+                href="/settings"
+                className={`${
+                  path === "/settings" ? "bg-gray-600" : "bg-gray-700"
+                } flex cursor-pointer flex-row items-center gap-2 rounded-md p-2  text-gray-100`}
+              >
+                <Cog8ToothIcon className="h-6 w-6" />
+                <p>Settings</p>
+              </Link>
+            </div>
+          </Menu.Item>
+
+          <Menu.Item>
+            <a
+              onClick={() => signOut()}
+              className={` flex cursor-pointer flex-row items-center gap-2 rounded-md p-2  text-gray-100`}
+            >
+              <ArrowLeftOnRectangleIcon className="h-6 w-6" />
+              <p>Logout</p>
+            </a>
+          </Menu.Item>
+        </Menu.Items>
+      </Menu>
+      <div className="ml-2 flex flex-col">
+        <p className="font-semibold text-gray-200">{name}</p>
+        <Link
+          //href={`/user/${id}`}
+          href={{
+            pathname: `/user/[id]`,
+            query: { id: id },
+          }}
+        >
+          <button
+            className="text-left text-sm text-gray-400 hover:text-gray-100"
+            id="view-profile-btn"
+          >
+            View Profile
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default AccountBox;
--- src/components/layouts/gridDashboard.tsx
+++ src/components/layouts/gridDashboard.tsx
@@ -24,6 +24,9 @@ import {
 
 import { env } from "~/env.mjs";
 
+import MobileAccountBox from "./components/MobileAccountBox";
+import AccountBox from "./components/AccountBox";
+
 // i guess i lost my sanity
 import Div100vh from "react-div-100vh";
 import SearchModal from "../SearchModal";
@@ -109,7 +112,7 @@ const GridDashboard: NextPage<{
 
             <div>
               {userData?.user?.image ? (
-                <MobilePhoto
+                <MobileAccountBox
                   image={userData?.user?.image ?? "/default_profile.jpg"}
                 />
               ) : (
@@ -193,138 +196,5 @@ const MenuItem = ({
         <p className="text-sm md:text-base">{text}</p>
       </div>
     </Link>
-  );
-};
-const AccountBox = ({
-  image,
-  name,
-  path,
-  id,
-  className,
-}: {
-  image: string;
-  name: string;
-  path: string;
-  id: string;
-  className?: string;
-}) => {
-  return (
-    <div
-      className={twMerge(
-        "flex",
-        className,
-        "mt-auto rounded-md bg-gray-700 p-2 "
-      )}
-      id="current-account-box"
-    >
-      <Menu>
-        <Menu.Button className="relative h-12 w-12 overflow-hidden rounded-full">
-          <Image
-            src={image || "/default_profile.jpg"}
-            layout="fill"
-            className="rounded-full"
-            alt="Profile Picture"
-          />
-        </Menu.Button>
-        <Menu.Items
-          className={`absolute bottom-0 -translate-y-20 rounded-md bg-gray-700 p-1 `}
-        >
-          <Menu.Item>
-            <div>
-              <Link
-                href="/profile"
-                className={`${
-                  path === "/profile" ? "bg-gray-600" : "bg-gray-700"
-                } flex cursor-pointer flex-row items-center gap-2 rounded-md p-2  text-gray-100`}
-              >
-                <UserCircleIcon className="h-6 w-6" />
-                <p>Profile</p>
-              </Link>
-            </div>
-          </Menu.Item>
-          <Menu.Item>
-            <div>
-              <Link
-                href="/settings"
-                className={`${
-                  path === "/settings" ? "bg-gray-600" : "bg-gray-700"
-                } flex cursor-pointer flex-row items-center gap-2 rounded-md p-2  text-gray-100`}
-              >
-                <Cog8ToothIcon className="h-6 w-6" />
-                <p>Settings</p>
-              </Link>
-            </div>
-          </Menu.Item>
-
-          <Menu.Item>
-            <a
-              onClick={() => signOut()}
-              className={` flex cursor-pointer flex-row items-center gap-2 rounded-md p-2  text-gray-100`}
-            >
-              <ArrowLeftOnRectangleIcon className="h-6 w-6" />
-              <p>Logout</p>
-            </a>
-          </Menu.Item>
-        </Menu.Items>
-      </Menu>
-      <div className="ml-2 flex flex-col">
-        <p className="font-semibold text-gray-200">{name}</p>
-        <Link
-          //href={`/user/${id}`}
-          href={{
-            pathname: `/user/[id]`,
-            query: { id: id },
-          }}
-        >
-          <button
-            className="text-left text-sm text-gray-400 hover:text-gray-100"
-            id="view-profile-btn"
-          >
-            View Profile
-          </button>
-        </Link>
-      </div>
-    </div>
-  );
-};
-const MobilePhoto = ({ image }: { image: string }) => {
-  return (
-    <Menu as="div" className={`self-stretch md:hidden`}>
-      <Menu.Button className={`relative h-10 w-10 focus:outline-none `}>
-        <Image
-          src={image || "/default_profile.jpg"}
-          alt="profile"
-          layout="fill"
-          className="aspect-square rounded-full"
-        ></Image>
-      </Menu.Button>
-      <Menu.Items className="absolute right-1 translate-y-3 rounded-md bg-gray-700 p-1 focus:outline-none active:outline-none">
-        <Link href={"/profile"}>
-          <Menu.Item>
-            <div className="flex cursor-pointer items-center gap-x-1 rounded p-1 align-middle text-white hover:bg-gray-800 active:bg-gray-800">
-              <UserCircleIcon className="h-5 w-5" />
-              <p className="h-fit max-h-fit">Profile</p>
-            </div>
-          </Menu.Item>
-        </Link>
-
-        <Link href="/settings">
-          <Menu.Item>
-            <div className="flex cursor-pointer items-center gap-x-1 rounded p-1 align-middle text-white hover:bg-gray-800 active:bg-gray-800">
-              <Cog8ToothIcon className="h-5 w-5"></Cog8ToothIcon>
-              <p className="h-fit max-h-fit">Settings</p>
-            </div>
-          </Menu.Item>
-        </Link>
-        <Menu.Item>
-          <div className="flex cursor-pointer items-center gap-x-1 rounded p-1 align-middle text-white hover:bg-gray-800 active:bg-gray-800">
-            <ArrowRightOnRectangleIcon className="h-5 w-5"></ArrowRightOnRectangleIcon>
-            <a className="max-h-min" onClick={() => signOut()}>
-              Logout
-            </a>
-          </div>
-        </Menu.Item>
-      </Menu.Items>
-    </Menu>
   );
 };
