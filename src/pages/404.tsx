8894fff096b41c67373384a18089f98dc10a7709
Momirtum <emirt398@gmail.com>
2023-11-11 17:27:21 +0300

    404 page height fixed

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -7,9 +7,8 @@ export default function Page404() {
       <Head>
         <title>404 - Page Not Found</title>
       </Head>
-      
-      
-      <main className=" grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+
+      <main className=" grid min-h-screen place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
         <div className="text-center">
           <p className="text-base font-semibold text-indigo-600">404</p>
           <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
