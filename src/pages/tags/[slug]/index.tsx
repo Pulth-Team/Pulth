5ba0ee529c58290ce6eb10c23064ed138948b45e
Bekir Gülestan <gulestanbekir@gmail.com>
2023-11-23 00:31:55 -0600

    Add TagSlug Page

---
+++ src/pages/tags/[slug]/index.tsx
@@ -0,0 +1,43 @@
+import { useRouter } from "next/router";
+import ArticleCard from "~/components/ArticleCard";
+import Dashboard from "~/components/layouts";
+import { api } from "~/utils/api";
+
+const TagSlugPage = () => {
+  const slug = useRouter().query.slug as string;
+
+  const articlesQuery = api.tag.searchArticlesByTag.useQuery({
+    tagSlug: slug,
+  });
+
+  return (
+    <Dashboard>
+      <p>&quot;{slug}&quot; articles:</p>
+
+      {articlesQuery.isLoading && <p>Loading...</p>}
+      {articlesQuery.isError && <p>Error</p>}
+      {articlesQuery.isSuccess && (
+        <div className="grid grid-cols-1 gap-4 p-4 md:grid-cols-2">
+          {articlesQuery.data?.map((article) => (
+            <ArticleCard
+              key={article.id}
+              Title={article.title}
+              slug={article.slug}
+              Author={{
+                Name: article.author.name || "no name",
+                UserId: article.author.id,
+                Image: article.author.image ?? undefined,
+              }}
+              createdAt={article.createdAt}
+              className="w-full max-w-none"
+            >
+              {article.description}
+            </ArticleCard>
+          ))}
+        </div>
+      )}
+    </Dashboard>
+  );
+};
+
+export default TagSlugPage;
